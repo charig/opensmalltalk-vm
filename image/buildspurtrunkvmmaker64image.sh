93385050d2c81d71641eb505a5ccf454610eb420
Guido Chari <charig@gmail.com>
2018-04-24 14:49:08 +0200

    ...

--- image/buildspurtrunkvmmaker64image.sh
+++ image/buildspurtrunkvmmaker64image.sh
@@ -4,10 +4,7 @@ set -e
 
 ./updatespur64image.sh "$@"
 
-cp -p $BASE64.image Spur64VMMaker.image
-cp -p $BASE64.changes Spur64VMMaker.changes
-
 . ./getGoodSpur64VM.sh
 
-echo $VM Spur64VMMaker.image BuildSqueakSpurTrunkVMMakerImage.st
-$VM Spur64VMMaker.image BuildSqueakSpurTrunkVMMakerImage.st
+echo $VM $BASE.image BuildSqueakSpurTrunkVMMakerImage.st
+$VM $BASE.image BuildSqueakSpurTrunkVMMakerImage.st
