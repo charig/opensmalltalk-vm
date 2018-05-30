1521d636dea2c259bd90dc68cf4ba2157fa9a9ea
Guido Chari <charig@gmail.com>
2018-05-30 17:44:57 +0200

    workaround vm name for linux

--- image/get64VMName.sh
+++ image/get64VMName.sh
@@ -2,20 +2,15 @@
 set -e
 set +v
 
-source envvars.sh
+. ./envvars.sh
 
-if [ "$1" = -vm -a -n "$2" -a -x "`which "$2"`" ]; then
-	VM="$2"
-else
-	case $OS in
-	Darwin) 
-		RELEASE=`find . -name 'squeak.cog.spur_macos64x64_*' | head -n 1 | sed -E "s/\.\/squeak.cog.spur_macos64x64_(.*).dmg/\1/"`
-		VM=Squeak64.$RELEASE.app
-		VM=$VM/Contents/MacOS/Squeak;;
-	Linux) # This needs to be split by $CPU to work on RPi also
-		# TODO: Adapt the sed command to linux squeak version
-		RELEASE=`find . -name 'squeak.cog.spur_macos64x64_*' | head -n 1 | sed -E "s/\.\/squeak.cog.spur_macos64x64_(.*).dmg/\1/"`
-		VM=sqlinux.$RELEASE
-		VM=$VM/squeak;;
-	esac
-fi
+case $OS in
+Darwin) 
+	RELEASE=`find . -name "squeak.cog.spur_macos64x64_*" | head -n 1 | sed -E 's/.\/squeak.cog.spur_macos64x64_(.*).dmg/\1/'`
+	VM=Squeak64.$RELEASE.app
+	VM=$VM/Contents/MacOS/Squeak;;
+Linux) 
+	RELEASE=`find . -name "sqlinux.*" | head -n 1 | sed -E 's/.\/sqlinux.(.*)/\1/'`
+	VM=sqlinux.$RELEASE
+	VM=$VM/squeak;;
+esac
